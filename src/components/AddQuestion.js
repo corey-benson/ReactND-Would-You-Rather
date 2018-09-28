d7e7d69fbf32c093e1328471ebfaf0cc01f4f8ac
Corey Benson <corey@corey.digital>
2018-09-28 15:00:52 +0100

    added add new question component

---
+++ src/components/AddQuestion.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class AddQuestion extends Component {
+  render() {
+    return (
+      <form className='add-form'>
+        <h3>What is your question?</h3>
+      </form>  
+    ) 
+  }
+}
+
+export default AddQuestion
--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
 import Dashboard from './Dashboard'
 import Leaderboard from './Leaderboard'
+import AddQuestion from './AddQuestion'
 import LoadingBar from 'react-redux-loading'
 
 class App extends Component {
@@ -16,7 +17,7 @@ class App extends Component {
         <LoadingBar />
         {this.props.loading === true
           ? null
-          : <Leaderboard />}
+          : <AddQuestion />}
       </div>
     )
   }
