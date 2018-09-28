2641f73a8a6e4771cf73f6e71f40c751d112bc74
Corey Benson <corey@corey.digital>
2018-09-28 14:26:05 +0100

    start displaying questions to the dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,10 +2,51 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class Dashboard extends Component {
+  state = {
+    showAnswered: false
+  }
+  showUnaswered = () => {
+    this.setState(() => ({
+      showAnswered: false
+    }))
+  }
+  showAnswered = () => {
+    this.setState(() => ({
+      showAnswered: true
+    }))
+  }
+
   render() {
-    console.log('all questions: ', this.props)
+    const { showAnswered } = this.state
+    const { answered, unanswered } = this.props
+
+    const allQuestions = showAnswered === true
+      ? answered
+      : unanswered
+
     return(
-      <div>Dashboard</div>
+      <div>
+        <div className='dashboard-toggle'>
+          <button
+            style={{ textDecoration: showAnswered === false ? 'underline' : null }}
+            onClick={this.showUnaswered}>
+            Unanswered
+          </button>
+          <span> | </span>
+          <button
+            style={{ textDecoration: showAnswered === true ? 'underline' : null }}
+            onClick={this.showAnswered}>
+            Answered
+          </button>
+        </div>
+        <ul className='dashboard-list'>
+          {allQuestions.map((question) => (
+            <li key={question.id}>
+              <em>Would You Rather</em> - {question.optionOne.text}
+            </li>
+          ))}
+        </ul>
+      </div>
     )
   }
 }
