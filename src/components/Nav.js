56a59a5602b7c54027d3d369ecd8da3162938b45
Corey Benson <corey@corey.digital>
2018-10-01 17:07:39 +0100

    added sign out functionality and updated Nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,25 +1,67 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-export default function Nav() {
-  return (
-    <nav className='nav'>
-      <ul>
-        <li>
-          <NavLink to='/' exact activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/leaderboard' activeClassName='active'>
-            Leaderboard
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/add' activeClassName='active'>
-            Add Question
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  )
+import React, { Component } from 'react'
+import { NavLink, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { logOut } from "../actions/authedUser"
+
+class Nav extends Component {
+  state = {
+    signOut: false
+  }
+
+  handleLogOut = (e) => {
+    e.preventDefault()
+
+    this.props.dispatch(logOut())
+    this.setState(() => ({
+      signOut: true
+    }))
+  }
+
+  render() {
+    const { currentUser} = this.props
+    const { signOut } = this.state
+
+    if (signOut) {
+      return (<Redirect to="/login" />)
+    }
+
+    return (
+      <nav className='nav'>
+        <ul>
+          <li>
+            <NavLink to='/' exact activeClassName='active'>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/leaderboard' activeClassName='active'>
+              Leaderboard
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/add' activeClassName='active'>
+              Add Question
+            </NavLink>
+          </li>
+          <li>
+            <span className='curr-user'><img className='avatar' src={currentUser.avatarURL} alt={currentUser.name} /> Hello, {currentUser.name}</span>
+          </li>
+          <li>
+            <NavLink to='#' activeClassName='active' onClick={this.handleLogOut}>Sign Out</NavLink>
+          </li>
+        </ul>
+      </nav>
+    )
+  }
 } 
+
+function mapStateToProps({ authedUser, users, id }) {
+  const currentUser = users[authedUser]
+
+  return {
+    currentUser,
+    user: users[id]
+  }
+}
+
+export default connect(mapStateToProps)(Nav)
