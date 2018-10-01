9f22dc623e0766a01ba647ea980e053e03467232
Corey Benson <corey@corey.digital>
2018-10-01 14:34:51 +0100

    added user login component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
+import UserLogin from './UserLogin'
 import Dashboard from './Dashboard'
 import LoadingBar from 'react-redux-loading'
 import Leaderboard from './Leaderboard'
@@ -27,7 +28,8 @@ class App extends Component {
             {this.props.loading === true
               ? null
               : <div>
-                <Route path='/' exact component={Dashboard} />
+                <Route path='/' exact component={UserLogin} />
+                {/* <Route path='/' exact component={Dashboard} /> */}
                 <Route path='/leaderboard' component={Leaderboard} />
                 <Route path='/questions/:id' component={Question} />
                 <Route path='/add' component={AddQuestion} />
---
+++ src/components/UserLogin.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class UserLogin extends Component {
+  render() {
+    return(
+      <div>User List</div>
+    )
+  }
+
+}
+
+export default connect()(UserLogin)
