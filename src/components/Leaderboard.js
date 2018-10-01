c138c44c2d3763602044d243419376f642664a04
Corey Benson <corey@corey.digital>
2018-10-01 21:04:16 +0100

    updated leaderboard score display

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,15 +1,31 @@
 import React from 'react'
 import { connect } from 'react-redux'
+
 function Leaderboard({ users }) {
   return (
     <ul>
       {users.map((user) => (
-        <li className='user' key={user.id}>
-          <img src={user.avatarURL} alt={`Avatar for ${user.name}`} />
-          <div>
-            <h1>{user.name}</h1>
-            <p>{user.questions} Questions</p>
-            <p>{user.answers} Answers</p>
+        <li 
+          className='user-entry' 
+          key={user.id}>
+          <div className='entry'>
+            <div className='entry-user'>
+              <div className='entry-user__avatar'>
+                <img src={user.avatarURL} alt={user.name} />
+              </div>
+              <div className='entry-user__name'>{user.name}</div>
+            </div>
+            <div className='entry-body'>
+              <div className='entry-body__content'>
+                <p className='asked'>You have asked <span>{user.questions}</span> questions.</p>
+              </div>
+              <div className='entry-body__content'>
+                <p className='answered'>And you have answered <span id='answered'>{user.answers}</span> questions.</p>
+              </div>
+              <div className='entry-body__score'>
+                With a total Score of:<br/><span>{user.questions + user.answers}</span>
+              </div>
+            </div>
           </div>
         </li>
       ))}
